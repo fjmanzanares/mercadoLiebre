fc8218a0fe75e4746d2164f85c201c5254b548cd
fjmanzanares <fran.manzanares@gmail.com>
2023-01-11 00:00:48 -0300

    "lalala"

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 
 const app = express();
-const publicPath = path.resolve(__dirname, "./public");
-app.use(express.static(publicPath));
 
-app.listen(3000, () => {
-  console.log("Up and running from 3000");
-});
+app.use( express.static(path.resolve(__dirname, './public')) );
 
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname + "/views/index.html"));
-});
+app.listen(3000, () =>  console.log("Up and running from 3000"));
+
+app.get('/', (req, res) => {res.sendFile(path.resolve(__dirname + '/views/index.html'));});
