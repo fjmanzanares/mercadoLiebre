db9e1bf1eed6ab5ff77f0f559ce163eafab952b1
fjmanzanares <fran.manzanares@gmail.com>
2023-04-24 09:15:48 -0300

    .

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const session = require('express-session');
 
 app.use( express.static(path.resolve(__dirname, './public')) );
 
-app.listen(3000, () =>  console.log("Up and running from 3000"));
+app.listen(3000, () =>  console.log("Servidor corriendo desde el puerto 3000"));
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname + '/views/index.html'));
