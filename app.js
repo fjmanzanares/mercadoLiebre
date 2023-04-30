4b1be38e1154ce46188c854244c81c790ddab253
fjmanzanares <fran.manzanares@gmail.com>
2023-04-30 13:47:24 -0300

    registro y login

--- app.js
+++ app.js
@@ -13,4 +13,12 @@ app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname + '/views/index.html'));
 });
 
+app.get('/registro', (req, res) => {
+    res.sendFile(path.resolve(__dirname + '/views/register.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname + '/views/login.html'));
+});
+
 app.use(session({secret: 'Shhhhhhhh'}));
